1ba5910bebbe2a8f1c3092126654c735d4f321fc
Steven G. Harms <steven.harms@flatironschool.com>
2019-05-08 14:00:15 -0400

    solution

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const testVar = {}
+function map(src, cb) {
+  let r = []
 
-function testFunc() {
-  return "hi"
+  for (let i = 0; i < src.length; i++) {
+    let theElement = src[i]
+    r.push(cb(theElement))
+  }
+
+  return r;
+}
+
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
 }
