a596ca81c55c9178b8969d4a5b9819f06b85a195
Liz Burton <liz.burton147@gmail.com>
2021-04-23 17:42:54 -0400

    Minor edit

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-function map(src, callBack) {
+function map(src, callback) {
   const r = [];
 
   for (let i = 0; i < src.length; i++) {
     const theElement = src[i];
-    r.push(callBack(theElement));
+    r.push(callback(theElement));
   }
 
   return r;
 }
 
-function reduce(src, callBack, startingValue) {
+function reduce(src, callback, startingValue) {
   let total;
   if (startingValue) {
     total = startingValue;
     for (let i = 0; i < src.length; i++) {
-      total = callBack(src[i], total);
+      total = callback(src[i], total);
     }
     return total;
   } else {
     total = src[0];
     for (let i = 1; i < src.length; i++) {
-      total = callBack(src[i], total);
+      total = callback(src[i], total);
     }
     return total;
   }
