1736d6047efb0931caca8e36418b3a15463aaebc
Blake A. Long <blake.a.long@gmail.com>
2021-04-23 15:22:58 -0600

    Updated "reduce" solution

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ function map(src, cb) {
   return r;
 }
 
-function reduce(src, cb, starting){
-  let r = (!!starting) ? starting : src[0]
-  let i = (!!starting) ? 0 : 1
-
-  for (; i < src.length; i++) {
-    r = cb(src[i], r)
+function reduce(src, cb, startingValue) {
+  let total 
+  if (startingValue) {
+    total = startingValue
+    for (let i = 0; i < src.length; i++) {
+      total = cb(src[i], total)
+    }
+    return total
+  } else {
+    total = src[0]
+    for (let i = 1; i < src.length; i++) {
+      total = cb(src[i], total)
+    }
+    return total
   }
-
-  return r;
 }
