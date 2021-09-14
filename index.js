141f9b738d350526027a01bd64c404461790c87e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-14 11:52:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+
+function a (array){
+    return array.map(element => -1 * element)
+}
+
+function b(array){
+    return array.map(element => element)
+}
+
+
+
+function map(array, fn){
+   return fn(array)
+}
+
